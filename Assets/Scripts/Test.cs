ce215b55b418f874e8eaaaf61481da038f046ec6
antaww <anta.pizzetta@gmail.com>
2023-03-09 18:14:27 +0100

    feat(assets): Add basics assets folders + downloaded assets (fonts & skies).

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+}
