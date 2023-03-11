8f9c760620ae0260826e801fb622f8cdfc32bc7b
antaww <anta.pizzetta@gmail.com>
2023-03-11 17:54:31 +0100

    feat(game): Add first tilemaps & player component.

---
+++ Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("PlayerMovements script attached to " + gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
